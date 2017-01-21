cbf0217072999525edc851f963b9c42bfdf99775
Magdusz <magdusz@MacBook-Pro-Magdusz.local>
2017-01-21 23:08:14 +0100

    Initial commit

---
+++ Assets/Scripts/CreateParticleSortingLayerFix.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateParticleSortingLayerFix : MonoBehaviour {
+
+	private ParticleSystemRenderer particleSystem;
+	// Use this for initialization
+	void Start () {
+		particleSystem = GetComponent<ParticleSystemRenderer> ();
+		particleSystem.sortingLayerName = "Player";
+		particleSystem.sortingOrder = -1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
---
+++ Assets/Scripts/SpeakerController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeakerController : MonoBehaviour {
+
+	public float jetpackForce = 75.0f;
+	public float forwardMovementSpeed = 3.0f;
+	private Rigidbody2D rigidbody2D;
+
+	public Transform groundCheckTransform;
+	private bool grounded;
+	public LayerMask groundCheckLayerMask;
+	Animator animator;
+
+	public ParticleSystem jetpack;
+
+	// Use this for initialization
+	void Start () {
+		rigidbody2D = GetComponent<Rigidbody2D>();
+		animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void FixedUpdate() {
+		bool jetpackActive = Input.GetKey("space");
+		if (jetpackActive) {
+			rigidbody2D.AddForce(new Vector2(0, jetpackForce));
+		}
+
+		Vector2 newVelocity = rigidbody2D.velocity;
+		newVelocity.x = forwardMovementSpeed;
+		rigidbody2D.velocity = newVelocity;
+
+		UpdateGroundedStatus ();
+		AdjustJetpack(jetpackActive);
+			
+	}
+	void UpdateGroundedStatus()
+	{
+		grounded = Physics2D.OverlapCircle(groundCheckTransform.position, 0.1f, groundCheckLayerMask);
+		animator.SetBool("grounded", grounded);
+	}
+
+	void AdjustJetpack (bool jetpackActive)
+	{
+		jetpack.enableEmission = !grounded;
+		jetpack.emissionRate = jetpackActive ? 300.0f : 75.0f; 
+	}
+		
+}
